382dac1fc78215987a103f638383d828c25cf4a8
lephuchung <lephuchung003@gmail.com>
2025-08-24 23:48:34 +0700

    Add cal logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainBinding: ActivityMainBinding
     var number: String? = null
 
+    var firstNumber: Double = 0.0
+    var lastNumber: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,46 +23,78 @@ class MainActivity : AppCompatActivity() {
 
         mainBinding.textViewResult.text = "0"
 
-        mainBinding.btnZero?.setOnClickListener {
+        mainBinding.btnZero.setOnClickListener {
             onNumberClicked("0");
         }
 
-        mainBinding.btnOne?.setOnClickListener {
+        mainBinding.btnOne.setOnClickListener {
             onNumberClicked("1");
         }
 
-        mainBinding.btnTwo?.setOnClickListener {
+        mainBinding.btnTwo.setOnClickListener {
             onNumberClicked("2");
         }
 
-        mainBinding.btnThree?.setOnClickListener {
+        mainBinding.btnThree.setOnClickListener {
             onNumberClicked("3");
         }
 
-        mainBinding.btnFour?.setOnClickListener {
+        mainBinding.btnFour.setOnClickListener {
             onNumberClicked("4");
         }
 
-        mainBinding.btnFive?.setOnClickListener {
+        mainBinding.btnFive.setOnClickListener {
             onNumberClicked("5");
         }
 
-        mainBinding.btnSix?.setOnClickListener {
+        mainBinding.btnSix.setOnClickListener {
             onNumberClicked("6");
         }
 
-        mainBinding.btnSeven?.setOnClickListener {
+        mainBinding.btnSeven.setOnClickListener {
             onNumberClicked("7");
         }
 
-        mainBinding.btnEight?.setOnClickListener {
+        mainBinding.btnEight.setOnClickListener {
             onNumberClicked("8");
         }
 
-        mainBinding.btnNine?.setOnClickListener {
+        mainBinding.btnNine.setOnClickListener {
             onNumberClicked("9");
         }
 
+        mainBinding.btnAC.setOnClickListener {
+            firstNumber = 0.0
+            lastNumber = 0.0
+        }
+
+        mainBinding.btnDel.setOnClickListener {
+
+        }
+
+        mainBinding.btnDevide.setOnClickListener {
+
+        }
+
+        mainBinding.btnMulti.setOnClickListener {
+
+        }
+
+        mainBinding.btnPlus.setOnClickListener {
+
+        }
+
+        mainBinding.btnMinus.setOnClickListener {
+
+        }
+
+        mainBinding.btnEqual.setOnClickListener {
+
+        }
+
+        mainBinding.btnDot.setOnClickListener {
+
+        }
     }
 
     fun onNumberClicked(clickedNumber: String){
@@ -69,6 +105,34 @@ class MainActivity : AppCompatActivity() {
         }
 
         mainBinding.textViewResult.text = number
+    }
+
+    fun plus(){
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber += lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun minus(){
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber -= lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun multiply(){
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        firstNumber *= lastNumber
+        mainBinding.textViewResult.text = firstNumber.toString()
+    }
+
+    fun divide(){
+        lastNumber = mainBinding.textViewResult.text.toString().toDouble()
+        if(lastNumber == 0.0)
+            Toast.makeText(applicationContext, "The divisor cannot be zero", Toast.LENGTH_LONG).show()
+        else{
+            firstNumber /= lastNumber
+            mainBinding.textViewResult.text = firstNumber.toString()
+        }
     }
 
 }
